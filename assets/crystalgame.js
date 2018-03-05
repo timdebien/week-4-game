0dcf2812a9e18496547dc0e597ffdfd9d3509c92
Timothy Debien <timothydebien@Timothys-MacBook-Pro.local>
2018-03-04 22:34:53 -0700

    final push

--- assets/crystalgame.js
+++ assets/crystalgame.js
@@ -1,70 +1,104 @@
-$(document).ready(function(){
+$(document).ready(function () {
+    var computerNumber;
 
-var computerNumber = Math.floor(Math.random()*112  + 19)
+    var numberOne;
+    var numberTwo;
+    var numberThree;
+    var numberFour;
 
-var numberOne = Math.floor(Math.random()*11 + 1)
-var numberTwo = Math.floor(Math.random()*11 + 1)
-var numberThree = Math.floor(Math.random()*11 + 1)
-var numberFour = Math.floor(Math.random()*11 + 1)
+    var wins = 0;
+    var losses = 0;
+    var humanScore;
 
-var wins = 0;
-var losses = 0;
-var humanScore = 0;
-$("#computer-score").html(computerNumber);
-$("#wins").html(wins);
-$("#losses").html(losses);
+    function reset() {
+        computerNumber = Math.floor(Math.random() * 112 + 19)
+        $("#computer-score").html(computerNumber);
+        numberOne = Math.floor(Math.random() * 11 + 1)
+        numberTwo = Math.floor(Math.random() * 11 + 1)
+        numberThree = Math.floor(Math.random() * 11 + 1)
+        numberFour = Math.floor(Math.random() * 11 + 1)
 
-$("#one").on ("click", function(){
-    humanScore = humanScore + numberOne;
-  
-    if(humanScore === computerNumber){
-    wins ++;
-}
-    else if(humanScore > computerNumber){
-    losses ++;
-}
-$("#my-score").html(humanScore);
+      
+        humanScore = 0;
+    }
+    reset();
 
-});
+    $("#computer-score").html(computerNumber);
+    $("#wins").html(wins);
+    $("#losses").html(losses);
+    $(".gempic").on("click", function(){
+        console.log('you clicked on', this);
+    })
+    $("#one").on("click", function () {
+        humanScore = humanScore + numberOne;
 
-$("#two").on ("click", function(){
-    humanScore = humanScore + numberTwo;
-    console.log("humanScore" + humanScore);
-    $("#my-score").html(humanScore);
-    
-    if(humanScore === computerNumber){
-    wins ++;
-}
-    else if(humanScore > computerNumber){
-    losses ++;
-}
+        if (humanScore === computerNumber) {
+            wins++;
+            reset();
+            $("#wins").html(wins);
+        }
+        else if (humanScore > computerNumber) {
+            losses++;
+            reset();
+            $("#losses").html(losses);
+        }
+        $("#my-score").html(humanScore);
+
+    });
+
+    $("#two").on("click", function () {
+        humanScore = humanScore + numberTwo;
+        console.log("humanScore" + humanScore);
+        $("#my-score").html(humanScore);
+
+        if (humanScore === computerNumber) {
+            wins++;
+            reset();
+            $("#wins").html(wins);
+        }
+        else if (humanScore > computerNumber) {
+            losses++;
+            reset();
+            $("#losses").html(losses);
+        }
 
 
-});
+    });
 
-$("#three").on ("click", function(){
-    humanScore = humanScore + numberThree;
-    console.log("humanScore" + humanScore);
-    
-    if(humanScore === computerNumber){
-    wins ++;
-}
-    else if(humanScore > computerNumber){
-    losses ++;
-}
-$("#my-score").html(humanScore);
-});
+    $("#three").on("click", function () {
+        humanScore = humanScore + numberThree;
+        console.log("humanScore" + humanScore);
 
-$("#four").on ("click", function(){
-    humanScore = humanScore + numberFour;
-    
-    if(humanScore === computerNumber){
-    wins ++;
-}
-    else if(humanScore > computerNumber){
-    losses ++;
-}
-$("#my-score").html(humanScore);
+        if (humanScore === computerNumber) {
+            wins++;
+            reset();
+            $("#wins").html(wins);
+        }
+        else if (humanScore > computerNumber) {
+            losses++;
+            reset();
+            $("#losses").html(losses);
+        }
+        $("#my-score").html(humanScore);
+    });
 
-});
+    $("#four").on("click", function () {
+        humanScore = humanScore + numberFour;
+
+        if (humanScore === computerNumber) {
+            wins++;
+            reset();
+            $("#wins").html(wins);
+
+        }
+        else if (humanScore > computerNumber) {
+            losses++;
+            reset();
+            $("#losses").html(losses);
+
+        }
+        $("#my-score").html(humanScore);
+
+    });
+   
 });
